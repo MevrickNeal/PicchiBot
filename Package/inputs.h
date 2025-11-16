70381ee6b9504195dfef9d18cfe300078ef74d0b
LianMollick <78555845+MevrickNeal@users.noreply.github.com>
2025-11-17 01:52:57 +0600

    Create inputs.h

---
+++ Package/inputs.h
@@ -0,0 +1,14 @@
+// -----------------------------
+// FILE: src/inputs.h
+// -----------------------------
+#ifndef INPUTS_H
+#define INPUTS_H
+
+
+namespace Inputs {
+void begin();
+void poll();
+}
+
+
+#endif // INPUTS_H
