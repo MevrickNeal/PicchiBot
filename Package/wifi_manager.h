ae63716ffb4ffcc52ddaa9082003c3b8fe31d7c0
LianMollick <78555845+MevrickNeal@users.noreply.github.com>
2025-11-17 01:56:07 +0600

    Create wifi_manager.h

---
+++ Package/wifi_manager.h
@@ -0,0 +1,13 @@
+// -----------------------------
+// FILE: src/wifi_manager.h
+// -----------------------------
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+
+namespace WiFiManager {
+void taskEntry(void*);
+}
+
+
+#endif // WIFI_MANAGER_H
