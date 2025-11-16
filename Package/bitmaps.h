1e4201ee25f89a1d7e538c33e91c446ff4e8c58d
LianMollick <78555845+MevrickNeal@users.noreply.github.com>
2025-11-17 02:11:18 +0600

    Create bitmaps.h

---
+++ Package/bitmaps.h
@@ -0,0 +1,17 @@
+// -----------------------------
+// FILE: src/bitmaps.h
+// -----------------------------
+#ifndef BITMAPS_H
+#define BITMAPS_H
+
+
+// Paste your full XBM arrays here (boot_anim_1, boot_anim_2, boot_anim_3, pet_anim)
+
+
+extern const unsigned char boot_anim_1[];
+extern const unsigned char boot_anim_2[];
+extern const unsigned char boot_anim_3[];
+extern const unsigned char pet_anim[];
+
+
+#endif // BITMAPS_H
